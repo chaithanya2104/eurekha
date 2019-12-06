723a73caa21a06bfc468e7bdf0fa5e502c88894c
training <kandulchaithanya21@gmail.com>
2019-12-06 16:25:33 -0600

    Initial commit

---
+++ src/test/java/com/example/eurekha/EurekhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
